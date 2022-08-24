b2660034d05c9f6dd83f5c9ef364734a1911f717
Marty <martynolan17@gmail.com>
2022-08-24 12:06:05 -0400

    connect to database/server + added user model

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Creates connection to database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "localhost", 
-    dialiect: "mysql",
+    dialect: "mysql",
     port: 3306
 });
 
