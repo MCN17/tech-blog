6556dd3cffc1f897f64f9d42811ab7a0f4d12b4d
Marty <martynolan17@gmail.com>
2022-08-27 15:15:41 -0400

    set up template engine

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment } = require("../../models");
 
 // All routes have been tested in Insomnia.
 
@@ -14,25 +14,43 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ["password"] },
-        where: {
-            id: req.params.id
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_content', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
+          model: Post,
+          attributes: ['title']
         }
+      ]
     })
-    .then(dbUserData => {
+      .then(dbUserData => {
         if (!dbUserData) {
-            res.status(404).json({ message: "No user found with this id." });
-            return;
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
         }
         res.json(dbUserData);
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+      });
+  });
 
 router.post("/", (req, res) => {
     User.create({
--- models/Post.js
+++ models/Post.js
@@ -18,12 +18,9 @@ Post.init(
             type: DataTypes.STRING, 
             allowNull: false
         },
-        post_url: {
-            type: DataTypes.STRING, 
-            allowNull: false, 
-            validate: {
-                isURL: true
-            }
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
         user_id: {
             type: DataTypes.INTEGER,
