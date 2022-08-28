451f7b65de1910b86af339ba91c7f833a63a9e37
Marty <martynolan17@gmail.com>
2022-08-28 16:53:16 -0400

    added login-page/created created session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
     Post.findAll({
       attributes: [
         "id",
@@ -34,6 +35,18 @@ router.get("/", (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+  router.get("/login", (req, res) => {
+
+    // redirect to homepage if once logged in
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+
+
+    res.render("login");
   });
 
 
--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+
 
 const app = express();
 const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+    secret: "Marty is the best!",
+    cookie: {}, 
+    resave: false, 
+    saveUninitialized: true, 
+    store: new SequelizeStore({
+        db: sequelize
+    })
+
+};
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -14,6 +29,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 // turns on routes
 app.use(routes);
@@ -22,3 +38,4 @@ app.use(routes);
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
+
